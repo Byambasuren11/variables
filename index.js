927aa1d0269035a7db9f1b30a35a495601f001a4
Byambasuren11 <byumbasurenbold@gmail.com>
2024-12-02 17:15:19 +0800

    new updtate

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("hello from variables")
+// 1. "Comments can make code readable" - нэг мөртэй коммент болгох
+// 2. Variable.js нэртэй файл үүсгэх, number, string, boolean, undefined, null зэрэг утгуудыг өгөх
+// 3. Datatypes.js нэртэй файл үүсгэх, typeof ашиглан өөр, өөр, төрлийн утгуудыг шалгах, number, string, boolean, undefined, null гэх мэт
+// 4. Утга оноогоогүй хувьсагч зарлах
+// 5. Утга оноосон хувьсагч зарлах
+// 6. Олон мөрөнд First name, last name, marital status, country, age гэсэн нэртэй хувьсагч зарлах
+// 7. Нэг мөрөнд First name, last name, marital status, country, age гэсэн нэртэй хувьсагч зарлах
+const firstName="Bold";
+console.log(firstName);
+const lastName="Byambasuren";
+console.log(lastName);
+const maritalStatus=false;
+console.log(maritalStatus);
+const country="Mongolia";
+console.log(country);
+const age=20;
+console.log(age);
+const name="byambasuren", ag=11, sta=false, coun="Mongolia";
+console.log(name, ag, sta, coun, ag);
